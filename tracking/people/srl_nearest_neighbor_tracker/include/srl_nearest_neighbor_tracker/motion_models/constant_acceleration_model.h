0d2d9f7d653034f1950a3ef4de055372404068e9
Natnael <natnael@kaist.ac.kr>
2019-03-06 17:13:25 +0900

    add constant acceleration model

---
+++ tracking/people/srl_nearest_neighbor_tracker/include/srl_nearest_neighbor_tracker/motion_models/constant_acceleration_model.h
@@ -0,0 +1,25 @@
+
+
+#include <srl_nearest_neighbor_tracker/motion_models/motion_model.h>
+#include <ros/ros.h>
+
+namespace srl_nnt{
+
+
+
+
+
+class ConstantAccelerationModel: Public MotionModel{
+    
+}
+
+
+
+
+
+
+
+
+
+
+}
